47ce8b05f7864841198f75c86724622c1671bb95
kalash <kalashsanoniya1818@gmail.com>
2025-12-06 18:24:33 +0530

    fix next.config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
   },
+
+  // warning fix for cross-origin requests in dev mode
+  allowedDevOrigins: ["http://192.168.80.1:3000"],
+
   compress: true,
   poweredByHeader: false,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
